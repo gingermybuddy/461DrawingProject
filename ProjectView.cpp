aabe2c088eddf1281a488dc24d6f72d4456fd22a
Alexander Wiecking <alexander.wiecking@live.longwood.edu>
2021-03-10 13:59:24 -0500

    Split the rectangle into its own function

--- ProjectView.cpp
+++ ProjectView.cpp
@@ -13,14 +13,8 @@ void ProjectView::change_tool(int tool)
 	m_tool = tool;
 }
 
-void ProjectView::mousePressEvent(QMouseEvent *event)
-{
-	//Grabs some useful information based on where the mouse was clicked.
-	QPoint start = event->pos();
-	QPointF q = mapToScene(start);
-	qreal x = q.x();
-	qreal y = q.y(); 
-
+void ProjectView::rect_tool(qreal x, qreal y)
+{		
 	QPen pen(Qt::black); //Sets up a basic pen
 	pen.setWidth(2);
 
@@ -30,7 +24,6 @@ void ProjectView::mousePressEvent(QMouseEvent *event)
 	r->setFlag(QGraphicsItem::ItemIsSelectable, true);
 	r->setFlag(QGraphicsItem::ItemIsMovable, true);
 	r->setCursor(Qt::PointingHandCursor);
-
 	r->setData(0, 0);
 	r->setData(1, "rect");
 	//This is some extra data we're adding to the item. Ideally the 'id' parameter
@@ -38,8 +31,20 @@ void ProjectView::mousePressEvent(QMouseEvent *event)
 	//The 'setData' function takes in an int as a key and a QVariant (any sort of variable) as parameters.
 	//In this case, we're using '0' as the key for the item's id.
 	//'1' is the key for the item's type.
+}
+
+void ProjectView::mousePressEvent(QMouseEvent *event)
+{
+	//Grabs some useful information based on where the mouse was clicked.
+	QPoint start = event->pos();
+	QPointF q = mapToScene(start);
+	qreal x = q.x();
+	qreal y = q.y(); 
+	
+	rect_tool(x, y); //Currently just calls the one function, should throw something to a tool class that calls the correct function.
 
 	//TO-DO LIST:
+	//Implement some sort of class that tracks what tool is being used and uses it on a mousePressEvent
 	//Create functions based on buttons for a selected tool (e.g. move the rectangle code to the 'Rectangle' button code)
 	//Set up color selection so it takes a color parameter (or takes whatever color is currently selected according to some var in ProjectView)
 
--- ProjectView.h
+++ ProjectView.h
@@ -1,6 +1,7 @@
 #include <QGraphicsView>
 #include <QAction>
 #include <QMouseEvent>
+#include <QPoint>
 
 #ifndef __PROJECTVIEW_H
 #define __PROJECTVIEW_H
@@ -12,6 +13,7 @@ class ProjectView : public QGraphicsView
 		int m_tool;
 	private slots:
 		void change_tool(int tool);
+		void rect_tool(qreal x, qreal y);
 	public:
 		void mousePressEvent(QMouseEvent* event);
 		ProjectView();
