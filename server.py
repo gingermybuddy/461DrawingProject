a4c394377eb654cca616510139782a7145947966
Jason Thompson <jason.thompson@live.longwood.edu>
2021-03-10 11:29:31 -0500

    Added some code in the server to get json files but not to send anything yet

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask import request
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods = ['POST', 'GET'])
+def getJsonFile():
+    jsonFile = request.get_json()
+    data = json.loads(jsonFile)
+
+if __name__ == '__main__':
+    app.run(debug=True)
