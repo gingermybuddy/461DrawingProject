a92486a65b3740d9772c3b5a42967d8a88c14dcd
Justin Moore <justin.moore1@live.longwood.edu>
2021-03-10 21:42:23 -0500

    Fixed some weird things with my vimrc so now my edits shoud not create tabbing inconsistencies

--- server.py
+++ server.py
@@ -9,27 +9,27 @@ def getJsonFile():
     data = json.loads(jsonFile)
 
 def addCircle(r,x,y): #Creates JSON-formatted circle data
-	return jsonify({'shape' : 'circle', 'data' : {'radius' : r, 'x' : x, 'y' : y}})
+    return jsonify({'shape' : 'circle', 'data' : {'radius' : r, 'x' : x, 'y' : y}})
 
 def addLine(x1,y1,x2,y2): #Creates JSON-formatted line data
-	return jsonify({'shape' : 'line', 'data' : {'start' : {'x' : x1, 'y' : y1}, 'end' : {'x' : x2, 'y' : y2}}})
+    return jsonify({'shape' : 'line', 'data' : {'start' : {'x' : x1, 'y' : y1}, 'end' : {'x' : x2, 'y' : y2}}})
 
 @app.route('/', methods = ['POST', 'GET'])
 def shapeType(): #Checks shape type received and calls matching function
-	shape = request.args.get('shape')
-	if shape == 'circle':
-		r = request.args.get('radius')
-		x = request.args.get('x')
-		y = request.args.get('y')
-		addCircle(r,x,y)
-	elif shape == 'line':
-		x1 = request.args.get('x1')
-		y1 = request.args.get('y1')
-		x2 = request.args.get('x2')
-		y2 = request.args.get('y2')
-		addLine(x1,y1,x2,y2)
-	else:
-		return jsonify({'error' : 'Bad Shape Type'})
+    shape = request.args.get('shape')
+    if shape == 'circle':
+        r = request.args.get('radius')
+        x = request.args.get('x')
+        y = request.args.get('y')
+        return addCircle(r,x,y)
+    elif shape == 'line':
+        x1 = request.args.get('x1')
+        y1 = request.args.get('y1')
+        x2 = request.args.get('x2')
+        y2 = request.args.get('y2')
+        return addLine(x1,y1,x2,y2)
+    else:
+        return jsonify({'error' : 'Bad Shape Type'})
 
 if __name__ == '__main__':
     app.run(debug=True)
