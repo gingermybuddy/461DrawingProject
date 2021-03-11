77a7a02abea94f6fedfabf78f3c456341193bb72
Cassandra Blackstone <blackstonecb@samwise.cs.longwood.edu>
2021-03-11 16:45:55 -0500

    Added final touches to be able to send data about the object in a post request to the server

--- ProjectScene.cpp
+++ ProjectScene.cpp
@@ -10,11 +10,10 @@ ProjectScene::ProjectScene() { }
 
 ProjectScene::~ProjectScene() { } 
 
+//sends the data about the object that was on the scene to the server. 
 void ProjectScene::sceneChanged(const
 		QList<QRectF> &region) {
 
-//This is a starter for sending a request to the server. It is not complete
-//yet.
 	
 	QNetworkAccessManager *manager = new QNetworkAccessManager(this);
 	QUrl url("http://127.0.0.1:5000/");
@@ -22,7 +21,7 @@ void ProjectScene::sceneChanged(const
 	request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
 
 	QUrlQuery params;
-    params.addQueryItem(tr("id"), tr("?"));
+//	params.addQueryItem(tr("id"), tr("?"));
 
         QList<QGraphicsItem*> changed_items;
         for (QRectF r : region) {
@@ -31,27 +30,16 @@ void ProjectScene::sceneChanged(const
 
     for (QGraphicsItem* i : changed_items) {
         params.addQueryItem(tr("shape"), i->data(1).toString());
-        params.addQueryItem(tr("x"), QString::number(i->scenePos().x()));
+    	params.addQueryItem(tr("radius"), QString::number(i->scenePos().x()/2));
+  	params.addQueryItem(tr("x"), QString::number(i->scenePos().x()));
         params.addQueryItem(tr("y"), QString::number(i->scenePos().y()));
     }
 
-
-
-/*
-    QList<QRectF> data = region->items();
-    for(QRectF i : data){
-        int x = i.x();
-        int y = i.y();
-
-       // QGraphicsRectItem* rect = dynamic_cast<QGraphicsRectItem *>(i);
-
-    }
-*/
 
 	connect(manager, SIGNAL(finished(QNetworkReply*)),this, SLOT(replyFinished(QNetworkReply*)));
 
 	manager->post(request, params.query(QUrl::FullyEncoded).toUtf8());
 	
 
-} //Network code and such goes here!
+}
 
