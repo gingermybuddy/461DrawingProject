814fa369ae70f40f6454b9fad54d925335813e14
Justin Moore <justin.moore1@live.longwood.edu>
2021-03-11 13:54:01 -0500

    I promise, my tabbing issues are going to stop now

--- server.py
+++ server.py
@@ -10,35 +10,35 @@ def getJsonFile():
     data = json.loads(jsonFile)
 
 def addCircle(r,x,y): #Creates JSON-formatted circle data
-	return jsonify({'shape' : 'circle', 'data' : {'radius' : r, 'x' : x, 'y' : y}})
+    return jsonify({'shape' : 'circle', 'data' : {'radius' : r, 'x' : x, 'y' : y}})
 
 def addLine(x1,y1,x2,y2): #Creates JSON-formatted line data
-	return jsonify({'shape' : 'line', 'data' : {'start' : {'x' : x1, 'y' : y1}, 'end' : {'x' : x2, 'y' : y2}}})
+    return jsonify({'shape' : 'line', 'data' : {'start' : {'x' : x1, 'y' : y1}, 'end' : {'x' : x2, 'y' : y2}}})
 
 def addRect (x,y,w,h): #Creates JSON-formatted rectangle data
-	return jsonify({'shape' : 'rect', 'data' : {'x' : x, 'y' : y, 'w' : w, 'h' : h}})
+    return jsonify({'shape' : 'rect', 'data' : {'x' : x, 'y' : y, 'w' : w, 'h' : h}})
 
 def shapeType(): #Checks shape type received and calls matching function
-	shape = request.args.get('shape')
-	if shape == 'circle':
-		r = request.args.get('radius')
-		x = request.args.get('x')
-		y = request.args.get('y')
-		return addCircle(r,x,y)
-	elif shape == 'line':
-		x1 = request.args.get('x1')
-		y1 = request.args.get('y1')
-		x2 = request.args.get('x2')
-		y2 = request.args.get('y2')
-		return addLine(x1,y1,x2,y2)
+    shape = request.args.get('shape')
+    if shape == 'circle':
+        r = request.args.get('radius')
+        x = request.args.get('x')
+        y = request.args.get('y')
+        return addCircle(r,x,y)
+    elif shape == 'line':
+        x1 = request.args.get('x1')
+        y1 = request.args.get('y1')
+        x2 = request.args.get('x2')
+        y2 = request.args.get('y2')
+        return addLine(x1,y1,x2,y2)
     elif shape == 'rect':
         x = request.args.get('x')
-		y = request.args.get('y')
-		w = request.args.get('width')
-		h = request.args.get('height')
+        y = request.args.get('y')
+        w = request.args.get('width')
+        h = request.args.get('height')
         return addRect(x,y,w,h)
-	else:
-		return jsonify({'error' : 'Bad Shape Type'})
+    else:
+        return jsonify({'error' : 'Bad Shape Type'})
 
 if __name__ == '__main__':
     app.run(debug=True)
