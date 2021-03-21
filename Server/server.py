22ba86afa309f1692acde0d74a19345e0ba342b5
Jason Thompson <jason.thompson@live.longwood.edu>
2021-03-21 15:14:42 -0400

    Add createBoard function that creates a new database for a new board

--- Server/server.py
+++ Server/server.py
@@ -6,6 +6,8 @@
 from flask import Flask, jsonify
 from flask import request
 import json
+import sqlite3
+import drawSvg as draw #Library used to generate an SVG image 
 
 app = Flask(__name__)
 
@@ -29,23 +31,36 @@ def addRect (x,y,w,h):
 #Checks shape type received and calls matching function
 @app.route('/', methods = ['POST', 'GET'])
 def shapeType():
+
+    #Connects to the database
+    boardId = request.args.get('id')
+    boardDBName = "board" + str(boardId) + ".db"
+
+    connection = sqlite3.connect(boardName)
+    cur = connection.cursor()
+
     shape = request.args.get('shape') #Grabs shape parameter and determines which shape it is; returns an error JSON if improper shape name
     if shape == 'circle':
         r = request.args.get('radius')
         x = request.args.get('x')
         y = request.args.get('y')
+        boardId = request.args.get('id')
+        userId = request.args.get('uid')
+        sqlCommand = """INSERT INTO circle VALUES (boardId, userId, x, y, r); """
         return addCircle(r,x,y)
     elif shape == 'line':
         x1 = request.args.get('x1')
         y1 = request.args.get('y1')
         x2 = request.args.get('x2')
         y2 = request.args.get('y2')
+        boardId = request.args.get('id')
         return addLine(x1,y1,x2,y2)
     elif shape == 'rect':
         x = request.args.get('x')
         y = request.args.get('y')
         w = request.args.get('width')
         h = request.args.get('height')
+        boardId = request.args.get('id')
         return addRect(x,y,w,h)
     else:
         return jsonify({'error' : 'Bad Shape Type'})
@@ -54,7 +69,35 @@ def shapeType():
 # User will send this url to the server and server will reply back with an SVG file
 @app.route('/saveFile', methods = ['POST', 'GET'])
 def saveFile():
-    barf = 5
+
+    # First get all the entries in the database and parse them out by shape
+    # Add differend if statements for each shape 
+
+    sqlCommand = """STUFF"""
+
+    return svgImageSaveFile
+
+
+@app.route('/createBoard', methods = ['POST', 'GET'])
+# Create a new database for every new board that is created
+def createBoard():
+    boardId = request.args.get('id')
+    boardDBName = "board" + str(boardId) + ".db"
+
+    connection = sqlite3.connect(boardName)
+    cur = connection.cursor()
+
+    # Create a table for every shape that will be present in the board
+    sqlCommand = """CREATE TABLE Circle (boardId int, userId int, radius double, x int, y int)"""
+    cur.execute(sqlCommand)
+
+    sqlCommand = """CREATE TABLE Line (boardId int, userId int, x1 int, x2 int, y1 int, y2 int)"""
+    cur.execute(sqlCommand)
+
+    sqlCommand = """CREATE TABLE Rect (boardId int, userId int, x int, y int, width int, height int)"""
+    cur.execute(sqlCommand)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
