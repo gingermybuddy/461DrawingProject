3d181590fe53e1704c56e0805a43759ab944eb54
eatinbread <ryan.crow@live.longwood.edu>
2021-03-22 18:47:08 -0400

    began implementation for global dict,NONFUNCTIONAL

--- Server/server.py
+++ Server/server.py
@@ -7,34 +7,43 @@ from flask import Flask, jsonify
 from flask import request
 import json
 
-shapes = []
+index = 0
+shapes = {}
 
 app = Flask(__name__)
 
-def fullUpdate():
-    return jsonify(shapes)
+#!! NON-FUNCTIONAL !!
 
 def getJsonFile():
     jsonFile = request.get_json()
     data = json.loads(jsonFile)
 
 #Creates JSON-formatted circle data
-def addCircle(r,x,y):
+def addCircle(r,x,y,index):
     circle = jsonify({'shape' : 'circle', 'data' : {'radius' : r, 'x' : x, 'y' : y}})
-    shapes += circle
+    circleJSON = "{'shape' : 'circle', 'data' : {'radius' : " + str(r) + ", 'x' : " + str(x) + "'y' : " + str(y) + "}}"
+    shapes[str(index)] = circleJSON
+    index += 1
+    print(index)
     return circle
 
 #Creates JSON-formatted line data
-def addLine(x1,y1,x2,y2):
+def addLine(x1,y1,x2,y2,index):
     line = jsonify({'shape' : 'line', 'data' : {'start' : {'x' : x1, 'y' : y1}, 'end' : {'x' : x2, 'y' : y2}}})
-    shapes += line
+    lineJSON = "{'shape' : 'line', 'data' : {'start' : {'x' :" + str(x1) + ", 'y' : " +  str(y1) + "}, 'end' : {'x' : " + str(x2) + ", 'y' : " + str(y2) + "}}}"
+    shapes[str(index)] = lineJSON
+    index += 1
+    print(index)
     return line
 
 #Creates JSON-formatted rectangle data
-def addRect (x,y,w,h):
+def addRect(x,y,w,h,index):
     rectangle = jsonify({'shape' : 'rect', 'data' : {'x' : x, 'y' : y, 'w' : w, 'h' : h}})
-    shapes += rectangle
+    rectangleJSON = "{'shape' : 'rect', 'data' : {'x' : " + str(x) + ", 'y' : " + str(y) + ", 'w' : " + str(w) + ", 'h' : " + str(h) + "}}"
+    shapes[str(index)] = rectangleJSON
+    index += 1
     return rectangle
+
 
 #Checks shape type received and calls matching function
 @app.route('/', methods = ['POST', 'GET'])
@@ -44,19 +53,21 @@ def shapeType():
         r = request.args.get('radius')
         x = request.args.get('x')
         y = request.args.get('y')
-        return addCircle(r,x,y)
+        return addCircle(r,x,y,index)
     elif shape == 'line':
         x1 = request.args.get('x1')
         y1 = request.args.get('y1')
         x2 = request.args.get('x2')
         y2 = request.args.get('y2')
-        return addLine(x1,y1,x2,y2)
+        return addLine(x1,y1,x2,y2,index)
     elif shape == 'rect':
         x = request.args.get('x')
         y = request.args.get('y')
         w = request.args.get('width')
         h = request.args.get('height')
-        return addRect(x,y,w,h)
+        return addRect(x,y,w,h,index)
+    elif shape == 'fullUpdate':
+        return jsonify(shapes)
     else:
         return jsonify({'error' : 'Bad Shape Type'})
 
