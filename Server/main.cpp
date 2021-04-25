4c4718ae2b5e6542c8643b0cc8f324d5f3141814
Wizard-Of-Chaos <59537242+Wizard-Of-Chaos@users.noreply.github.com>
2021-04-25 15:01:26 -0400

    Added a thing to prompt you to specify a port number if you didn't

--- Server/main.cpp
+++ Server/main.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char* argv[])
 {
+    if(argc < 2) {
+        std::cout << "Please specify a port." << std::endl;
+        return -1;
+    }
 	QApplication new_window(argc, argv);
     Server w(std::stoi(argv[1]));
     return new_window.exec();
